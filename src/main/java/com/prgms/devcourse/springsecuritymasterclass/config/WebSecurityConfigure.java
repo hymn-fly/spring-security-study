47f8b40b6e62340656116967af11d17fab44f4d1
hymn-fly <rlfrmsdh1@gmail.com>
2022-05-17 16:02:20 +0900

    추가 - SpringSecurity config. formlogin, rememberme

--- src/main/java/com/prgms/devcourse/springsecuritymasterclass/config/WebSecurityConfigure.java
+++ src/main/java/com/prgms/devcourse/springsecuritymasterclass/config/WebSecurityConfigure.java
@@ -16,21 +16,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
-@EnableWebSecurity
+import static java.lang.String.format;
+
+@EnableWebSecurity(debug = true)
 @Configuration
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        String idForEncode = "noOp";
-        Map<String,PasswordEncoder> encoders = new HashMap<>();
-        encoders.put(idForEncode, NoOpPasswordEncoder.getInstance());
-        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        String idForEncode = "noop";
 
         auth.inMemoryAuthentication()
-                .passwordEncoder(new DelegatingPasswordEncoder("noop", encoders))
-                .withUser("user").password("user123").roles("USER").and()
-                .withUser("admin").password("admin123").roles("ADMIN");
+                .withUser("user").password(format("{%s}user123", idForEncode)).roles("USER").and()
+                .withUser("admin").password(format("{%s}admin123", idForEncode)).roles("ADMIN");
     }
 
     @Override
@@ -45,8 +43,15 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .antMatchers("/me").hasAnyRole("USER", "ADMIN")
                 .anyRequest().permitAll()
                 .and()
-                .formLogin()
+            .formLogin()
                 .defaultSuccessUrl("/")
-                .permitAll();
+                .permitAll()
+                .and()
+            .logout()
+                .logoutSuccessUrl("/")
+                .and()
+            .rememberMe().tokenValiditySeconds(300);
     }
+
+
 }

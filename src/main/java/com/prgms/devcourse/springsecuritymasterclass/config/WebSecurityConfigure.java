7e3f498a7a60a9c37d8fd12ef20462017ff5f112
hymn-fly <rlfrmsdh1@gmail.com>
2022-05-19 15:03:41 +0900

    추가 - CustomAccessDeniedHandler, 인증서 추가

--- src/main/java/com/prgms/devcourse/springsecuritymasterclass/config/WebSecurityConfigure.java
+++ src/main/java/com/prgms/devcourse/springsecuritymasterclass/config/WebSecurityConfigure.java
@@ -1,7 +1,11 @@
 package com.prgms.devcourse.springsecuritymasterclass.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,10 +13,18 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +33,8 @@ import static java.lang.String.format;
 @EnableWebSecurity(debug = true)
 @Configuration
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -41,16 +55,46 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/admin").access("hasRole('ADMIN')")
                 .anyRequest().permitAll()
                 .and()
             .formLogin()
+                .usernameParameter("my-username")
+                .passwordParameter("my-password")
                 .defaultSuccessUrl("/")
                 .permitAll()
                 .and()
             .logout()
                 .logoutSuccessUrl("/")
                 .and()
-            .rememberMe().tokenValiditySeconds(300);
+            .rememberMe().tokenValiditySeconds(300)
+                .key("my-remember-key")
+                .rememberMeParameter("remember")
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler()).and()
+//                .sessionManagement()
+//                .sessionFixation().changeSessionId()
+//                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+//                .invalidSessionUrl("/")
+//                .maximumSessions(1)
+//                    .maxSessionsPreventsLogin(false);
+        ;
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler(){
+        return (request, response, accessDeniedException) -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principal = authentication != null ? authentication.getPrincipal() : null;
+            log.warn("{} is denied", principal, accessDeniedException);
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("text/plain");
+            response.getWriter().write("## ACCESS DENIED ##");
+            response.getWriter().flush();
+            response.getWriter().close();
+        };
+
     }
 
 

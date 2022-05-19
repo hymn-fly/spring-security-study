87cee4f9e93a5c40f7f4b6e32b3248498951d862
hymn-fly <rlfrmsdh1@gmail.com>
2022-05-19 15:55:32 +0900

    추가  - CustomVoter정의 & 등록하여 사용

--- src/main/java/com/prgms/devcourse/springsecuritymasterclass/config/OddAdminVoter.java
+++ src/main/java/com/prgms/devcourse/springsecuritymasterclass/config/OddAdminVoter.java
@@ -3,16 +3,19 @@ package com.prgms.devcourse.springsecuritymasterclass.config;
 import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.FilterInvocation;
-import org.springframework.security.web.access.expression.WebExpressionConfigAttribute;
 
 import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class OddAdminVoter implements AccessDecisionVoter<FilterInvocation> {
+    private static final Pattern PATTERN = Pattern.compile("[0-9]$");
 
     @Override
     public boolean supports(ConfigAttribute attribute) {
-        return attribute instanceof WebExpressionConfigAttribute;
+        return false;
     }
 
     @Override
@@ -22,6 +25,18 @@ public class OddAdminVoter implements AccessDecisionVoter<FilterInvocation> {
 
     @Override
     public int vote(Authentication authentication, FilterInvocation object, Collection<ConfigAttribute> attributes) {
-        return 0;
+        if(!object.getRequestUrl().equals("/admin"))
+            return ACCESS_GRANTED;
+        User user = (User)authentication.getPrincipal();
+        String username = user.getUsername();
+        Matcher matcher = PATTERN.matcher(username);
+        if(matcher.find()){
+            int number = Integer.parseInt(matcher.group());
+            if(number % 2 != 0){
+                return ACCESS_GRANTED;
+            }
+            return ACCESS_DENIED;
+        }
+        return ACCESS_ABSTAIN;
     }
 }

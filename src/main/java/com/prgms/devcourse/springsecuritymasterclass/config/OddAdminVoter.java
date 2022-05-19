a4b592fa3e95700f0f9faca19bc4d4b031975944
hymn-fly <rlfrmsdh1@gmail.com>
2022-05-19 18:35:26 +0900

    추가  - OddAdminVoter 리팩토링

--- src/main/java/com/prgms/devcourse/springsecuritymasterclass/config/OddAdminVoter.java
+++ src/main/java/com/prgms/devcourse/springsecuritymasterclass/config/OddAdminVoter.java
@@ -5,7 +5,9 @@ import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,9 +15,15 @@ import java.util.regex.Pattern;
 public class OddAdminVoter implements AccessDecisionVoter<FilterInvocation> {
     private static final Pattern PATTERN = Pattern.compile("[0-9]$");
 
+    private final RequestMatcher requestMatcher;
+
+    public OddAdminVoter(RequestMatcher requestMatcher) {
+        this.requestMatcher = requestMatcher;
+    }
+
     @Override
     public boolean supports(ConfigAttribute attribute) {
-        return false;
+        return true;
     }
 
     @Override
@@ -25,7 +33,7 @@ public class OddAdminVoter implements AccessDecisionVoter<FilterInvocation> {
 
     @Override
     public int vote(Authentication authentication, FilterInvocation object, Collection<ConfigAttribute> attributes) {
-        if(!object.getRequestUrl().equals("/admin"))
+        if(!requiresAuthorization(object.getRequest()))
             return ACCESS_GRANTED;
         User user = (User)authentication.getPrincipal();
         String username = user.getUsername();
@@ -38,5 +46,9 @@ public class OddAdminVoter implements AccessDecisionVoter<FilterInvocation> {
             return ACCESS_DENIED;
         }
         return ACCESS_ABSTAIN;
+    }
+
+    private boolean requiresAuthorization(HttpServletRequest request){
+        return requestMatcher.matches(request);
     }
 }
